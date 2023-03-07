5969ede8f2efe6a2f3d3741bcf673bd454b9ffc5
White-OvO <christophergonzalez1996@yahoo.com>
2023-03-07 15:51:37 -0800

    DELETE YOU GOT THIS

---
+++ src/WE/method_examples.java
@@ -0,0 +1,37 @@
+package WE;
+
+public class method_examples {
+
+	public static void main(String[] args) {
+	
+		welcome() ;
+		System.out.println("The multiplication side of the calculator");
+		multiply(10,10);
+		multiply(50,200);
+		System.out.println("Example of a division method");
+		divison(300, 10);
+	// this is a method being called upon again 
+		welcome();
+	}
+	public static void multiply(int a , int b) {
+	System.out.println(a * b);
+	}
+	
+	/// neww method for string
+	public static void welcome () {
+		System.out.println("Welcome to our multiplication calculator");
+	}
+		
+		// Now a division calculator
+	public static void divison (int a, int b) {
+	System.out.println(a / b);
+	{
+	}
+			
+			
+	}
+	}
+	
+	
+
+
---
+++ src/WE/preview.java
@@ -0,0 +1,5 @@
+package WE;
+
+public class preview {
+
+}
